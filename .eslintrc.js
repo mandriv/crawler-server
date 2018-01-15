35fa2304f242bfb74861b27e2c0af6fe34420c1c
mandriv <wojciech.cichoradzki@gmail.com>
2018-01-15 22:28:20 +0000

    Updated linter rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,12 @@
 module.exports = {
-    "extends": "airbnb-base"
+    "extends": "airbnb-base",
+    "parser": "babel-eslint",
+    "rules": {
+      "no-console": ["error", { allow: ["warn", "error"] }],
+      "padded-blocks": ["error", {
+        "classes": "always",
+        "switches": "never",
+        "blocks": "never",
+      }],
+    },
 };
