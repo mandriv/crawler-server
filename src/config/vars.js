69b9ae77db8fd95ee1ca2cc729adfeb9e2a15ffe
mandriv <wojciech.cichoradzki@gmail.com>
2018-01-16 21:57:55 +0000

    Updated mongo uri

--- src/config/vars.js
+++ src/config/vars.js
@@ -1,4 +1,4 @@
-export const MONGO_URI = 'mongodb://localhost/crawler';
+export const MONGO_URI = 'mongodb://[mandriv:Zh%3PNdHn@3TcsOnu7VWXf!b$P$S1dOJc$Z^brRWF8kNr*sT5VbI3mRCFriS^XYx]localhost/crawler';
 export const PORT = 3000;
 export const API_VERSION = 1;
 export const API_PREFIX = `/api/v${API_VERSION}/`;
