fba2cb7811234d6ef1ef1c99b7c22eec72bcb7e6
mandriv <wojciech.cichoradzki@gmail.com>
2018-02-02 18:56:42 +0000

    Added email verification model

---
+++ src/models/verification.model.js
@@ -0,0 +1,30 @@
+import mongoose from 'mongoose';
+import uuidv4 from 'uuid/v4';
+
+/**
+ * Verification Schema
+ * Used for verification of email address
+ */
+
+const { Schema } = mongoose;
+const VerificationSchema = new Schema({
+  token: {
+    type: String,
+    required: [true, 'Token is required'],
+  },
+  user: [{
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+  }],
+}, {
+  timestamps: true,
+  strict: true,
+});
+
+VerificationSchema.statics = {
+  generateToken() {
+    return uuidv4();
+  },
+};
+
+export default mongoose.model('Verification', VerificationSchema);
