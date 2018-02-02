46cd3aba22afc9ece518b5d1677042086800a1ff
mandriv <wojciech.cichoradzki@gmail.com>
2018-02-02 18:57:46 +0000

    Refactored responses format in login

--- src/controllers/login.controller.js
+++ src/controllers/login.controller.js
@@ -12,18 +12,18 @@ class LoginController extends Controller {
   login = async (req, res, next) => {
     const params = this.filterParams(req.body, this.whitelist);
     if (!params.email) {
-      res.status(400).json({ err: true, msg: 'Email is required' });
+      res.status(400).json({ error: 'Email is required' });
     }
     if (!params.password) {
-      res.status(400).json({ err: true, msg: 'Password is required' });
+      res.status(400).json({ error: 'Password is required' });
     }
     try {
       const user = await User.findOne({ email: params.email });
       if (!user || !user.authenticate(params.password)) {
-        res.status(401).json({ err: true, msg: 'Invalid email or password' });
+        res.status(401).json({ err: 'Invalid email or password' });
       }
       const token = user.generateToken();
-      res.status(200).json({ err: false, token });
+      res.status(200).json({ token });
     } catch (err) {
       next(err);
     }
