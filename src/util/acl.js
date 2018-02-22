0be0fdce70cc72b27fc6f1a6c9498d24cf78b449
mandriv <wojciech.cichoradzki@gmail.com>
2018-02-22 22:00:23 +0000

    Added acl permissions for crawlers

--- src/util/acl.js
+++ src/util/acl.js
@@ -15,11 +15,23 @@ const grantsObject = {
       'update:any': ['*'],
       'delete:any': ['*'],
     },
+    crawlers: {
+      'create:any': ['*'],
+      'read:any': ['*'],
+      'update:any': ['*'],
+      'delete:any': ['*'],
+    },
   },
   user: {
     // users resource
     users: {
       'create:any': ['*'],
+      'read:own': ['*'],
+      'update:own': ['*'],
+      'delete:own': ['*'],
+    },
+    crawlers: {
+      'create:own': ['*'],
       'read:own': ['*'],
       'update:own': ['*'],
       'delete:own': ['*'],
