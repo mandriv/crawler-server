e1e67d152e2dd03eea3167cba22bc3a22833988e
mandriv <wojciech.cichoradzki@gmail.com>
2018-02-22 21:58:21 +0000

    Removed unrelated code from institution controller

--- src/controllers/institution.controller.js
+++ src/controllers/institution.controller.js
@@ -3,32 +3,6 @@ import User from '../models/user.model';
 
 class InstitutionController extends Controller {
 
-  whitelist = [
-    'email',
-    'password',
-  ];
-
-  // POST /login
-  update = async (req, res, next) => {
-    const params = this.filterParams(req.body, this.whitelist);
-    if (!params.email) {
-      res.status(400).json({ err: true, msg: 'Email is required' });
-    }
-    if (!params.password) {
-      res.status(400).json({ err: true, msg: 'Password is required' });
-    }
-    try {
-      const user = await User.findOne({ email: params.email });
-      if (!user || !user.authenticate(params.password)) {
-        res.status(401).json({ err: true, msg: 'Invalid email or password' });
-      }
-      const token = user.generateToken();
-      res.status(200).json({ err: false, token });
-    } catch (err) {
-      next(err);
-    }
-  }
-
 }
 
 export default new InstitutionController();
