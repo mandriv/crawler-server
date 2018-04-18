fa29071809e4119dbdf822d63f653980798fc8f6
mandriv <wojciech.cichoradzki@gmail.com>
2018-04-18 18:30:47 +0100

    Strip down socket handling to use native io methods for retrieving rooms

--- services/RobotControl/src/server.js
+++ services/RobotControl/src/server.js
@@ -3,33 +3,26 @@ import express from 'express';
 import SocketIO from 'socket.io';
 
 import envCheck from './util/envCheck'; // eslint-disable-line
+import * as pool from './util/pool';
 
 const app = express();
 const server = http.Server(app);
-const io = new SocketIO(server);
+const io = new SocketIO(server, {
+  path: '/robot-control',
+});
 
 // settings
 app.set('trust proxy', true);
 
-// routes
-app.use('/', apiRouter);
-
 // sockets.io handling
-const pool = new RoomsPool();
 
 io.on('connection', (socket) => {
   // Robot join
   socket.on('robot-join', (robot) => {
-    console.log(`Robot ${robot.id} joined!`);
+    console.log(`Robot ${robot.id} joined!`); // eslint-disable-line
     socket.isRobot = true; // eslint-disable-line
     socket.robot = robot; // eslint-disable-line
     const roomName = `control-${robot.id}`;
-    let newRoom = pool.getRoomByName(roomName);
-    if (!newRoom) {
-      newRoom = new Room(roomName);
-    }
-    newRoom.joinRobot(robot);
-    pool.createRoom(newRoom);
     socket.join(roomName);
     socket.broadcast.emit('room-list-update');
   });
@@ -39,43 +32,44 @@ io.on('connection', (socket) => {
       socket.emit('user-join-fail', 'You need to pass user object!');
       return;
     }
-    console.log(`User ${user.id} joined!`);
-    socket.isRobot = false; // eslint-disarsble-line
+    console.log(`User ${user.id} joined!`); // eslint-disable-line
+    socket.isRobot = false; // eslint-disable-line
     socket.user = user; // eslint-disable-line
   });
   // List rooms
   socket.on('request-room-list', () => {
     if (!socket.isRobot && socket.user) {
-      console.log('sending room list');
+      console.log('sending room list'); // eslint-disable-line
       socket.emit('room-list', pool.getAvailableRooms(socket.user));
     } else {
-      console.log('sending error with room list');
+      console.log('sending error with room list'); // eslint-disable-line
       socket.emit('request-room-fail', 'Send \'user-join\' request first!');
     }
   });
   // User join room
   socket.on('user-join-room', (roomName) => {
     if (!socket.user) {
+      console.log(`Failed to join room ${roomName} - no user object in request`); // eslint-disable-line
       socket.emit('user-join-room-fail', 'Send \'user-join\' request first!');
       return;
     }
-    const room = pool.getRoomByName(roomName);
-    if (room) {
-      room.joinUser(socket.user);
-      socket.join(roomName);
+    if (pool.isValidRoom(roomName)) {
       socket.room = roomName; // eslint-disable-line
+      socket.join(roomName);
       socket.broadcast.emit('room-list-update');
-      console.log(`user joined room: ${roomName}`);
+      console.log(`user joined room: ${roomName}`); // eslint-disable-line
     } else {
+      console.log(`Failed to join room ${roomName} - Incorrect room name`); // eslint-disable-line
       socket.emit('user-join-room-fail', 'Incorrect room name!');
     }
   });
   // Send control data
   socket.on('robot-control', (data) => {
-    console.log(socket.room);
-    console.log(data);
+    console.log(data); // eslint-disable-line
     io.sockets.in(socket.room).emit('robot-control', data);
   });
+  /*
+  // TODO: Move to separate microservice
   // video stream
   socket.on('video-stream', (data) => {
     // stream buffer to the individual robot's stream room
@@ -83,28 +77,15 @@ io.on('connection', (socket) => {
   });
   // join video stream room
   socket.on('join-stream', id => socket.join(id))
+  */
   // Disconnect
   socket.on('disconnect', () => {
-    let room;
-    if (socket.isRobot && socket.robot && socket.robot.id) {
-      console.log(`Robot ${socket.robot.id} leaving`);
-      room = pool.findRobotsRoom(socket.robot);
-      if (room) {
-        room.leaveRobot();
-      }
-    } else if (!socket.isRobot && socket.user && socket.user.id) {
-      console.log(`User ${socket.user.id} leaving`);
-      room = pool.findUsersRoom(socket.user);
-      if (room) {
-        room.leaveUser();
-      }
-    }
-    if (room && room.isEmpty()) {
-      pool.removeRoomById(room.id);
-      socket.leave(room.name);
-    }
     socket.broadcast.emit('room-list-update');
   });
 });
 
-// initialization
+// start listnening
+const port = process.env.PORT
+server.listen(port, () => {
+  console.log(`Ready to receive robot controls on port ${port}!`); // eslint-disable-line
+});
---
+++ services/RobotControl/src/util/pool.js
@@ -0,0 +1,5 @@
+// Helper functions for handling multi room socket connections
+export const getAvailableRooms = () => {
+
+};
+export const isValidRoom = () => [];
