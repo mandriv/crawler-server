b767b9a9305556259076acdefe7909b1b5da2d55
mandriv <wojciech.cichoradzki@gmail.com>
2018-04-19 23:01:23 +0100

    Initial WebSocket video streaming server implementation

--- services/VideoStreams/src/server.js
+++ services/VideoStreams/src/server.js
@@ -1,90 +1,22 @@
 import http from 'http';
 import express from 'express';
-import SocketIO from 'socket.io';
+import WebSocket from 'ws';
 
 import envCheck from './util/envCheck'; // eslint-disable-line
-import * as pool from './util/pool';
 
 const app = express();
 const server = http.Server(app);
-const io = new SocketIO(server, {
-  path: '/robot-control',
-});
+const wss = new WebSocket.Server({ server });
 
-// sockets.io handling
-io.set('destroy upgrade', false);
-io.set('transports', ['websocket']);
-
-io.on('connection', (socket) => {
-  // Robot join
-  socket.on('robot-join', (robot) => {
-    console.log(`Robot ${robot.id} joined!`); // eslint-disable-line
-    socket.isRobot = true; // eslint-disable-line
-    socket.robot = robot; // eslint-disable-line
-    const roomName = `control-${robot.id}`;
-    socket.join(roomName);
-    socket.broadcast.emit('room-list-update');
-  });
-  // User join
-  socket.on('user-join', (user) => {
-    if (!user) {
-      socket.emit('user-join-fail', 'You need to pass user object!');
-      return;
-    }
-    console.log(`User ${user.id} joined!`); // eslint-disable-line
-    socket.isRobot = false; // eslint-disable-line
-    socket.user = user; // eslint-disable-line
-  });
-  // List rooms
-  socket.on('request-room-list', () => {
-    if (!socket.isRobot && socket.user) {
-      console.log('sending room list'); // eslint-disable-line
-      socket.emit('room-list', pool.getAvailableRooms(socket.user));
-    } else {
-      console.log('sending error with room list'); // eslint-disable-line
-      socket.emit('request-room-fail', 'Send \'user-join\' request first!');
-    }
-  });
-  // User join room
-  socket.on('user-join-room', (roomName) => {
-    if (!socket.user) {
-      console.log(`Failed to join room ${roomName} - no user object in request`); // eslint-disable-line
-      socket.emit('user-join-room-fail', 'Send \'user-join\' request first!');
-      return;
-    }
-    if (pool.isValidRoom(roomName)) {
-      socket.room = roomName; // eslint-disable-line
-      socket.join(roomName);
-      socket.broadcast.emit('room-list-update');
-      console.log(`user joined room: ${roomName}`); // eslint-disable-line
-    } else {
-      console.log(`Failed to join room ${roomName} - Incorrect room name`); // eslint-disable-line
-      socket.emit('user-join-room-fail', 'Incorrect room name!');
-    }
-  });
-  // Send control data
-  socket.on('robot-control', (data) => {
-    console.log(data); // eslint-disable-line
-    io.sockets.in(socket.room).emit('robot-control', data);
-  });
-  /*
-  // TODO: Move to separate microservice
-  // video stream
-  socket.on('video-stream', (data) => {
-    // stream buffer to the individual robot's stream room
-    io.sockets.in(`stream-${data.robotID}`).emit('video-stream', data.buffer);
-  });
-  // join video stream room
-  socket.on('join-stream', id => socket.join(id))
-  */
-  // Disconnect
-  socket.on('disconnect', () => {
-    socket.broadcast.emit('room-list-update');
+wss.on('connection', (ws) => {
+  ws.on('video-stream', (data) => {
+    console.log('received data');
+    console.log(data);
   });
 });
 
 // start listnening
 const port = process.env.PORT
 server.listen(port, () => {
-  console.log(`Ready to receive robot controls on port ${port}!`); // eslint-disable-line
+  console.log(`Waiting for video streams on port ${port}!`); // eslint-disable-line
 });
